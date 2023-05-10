180607f95cff8c0b5a4b029562351f1768d9f6e3
Maycon <maykonlemos2@gmail.com>
2023-05-09 22:07:13 -0300

    Domain model

--- src/main/java/com/devsuperior/dslist/entities/Game.java
+++ src/main/java/com/devsuperior/dslist/entities/Game.java
@@ -8,6 +8,7 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+
 @Entity
 @Table(name = "tb_game" )
 public class Game {
